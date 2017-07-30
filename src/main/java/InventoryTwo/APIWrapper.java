dd39b7b25507f21d5acbce2fd9f7c7903f4a40cd
Andrew Scales <code@Ambiguous-MacBook-Pro.local>
2017-07-30 16:06:29 -0400

    Framework/Skeleton for advanced features

---
+++ src/main/java/InventoryTwo/APIWrapper.java
@@ -0,0 +1,12 @@
+package InventoryTwo;
+
+/**
+ * Created by code on 7/30/17.
+ */
+public final class APIWrapper {
+
+    //All the things that make the API work go here.
+    //TODO: ADD ALL THE THINGS
+
+    //TODO: Consider writing the DB write methods here. Or writing another class for it. Allowing for more pluggability in the long run.
+}
---
+++ src/main/java/InventoryTwo/Transfer.java
@@ -0,0 +1,7 @@
+package InventoryTwo;
+
+/**
+ * Created by code on 7/30/17.
+ */
+public class Transfer {
+}
